8945e1362e8d6ee8773a861ae68bb545ed4acb63
lynn <358812977@163.com>
2020-02-25 00:08:56 +0800

    新增： 1.模块化组织整体代码； 2.3种模型效果的对比； 3.能够输回到原arff文件中；
    解决：
    1.不能识别string的问题；
    2.模型少的问题。

--- src/main/java/Algorithm/OAClassifyApplication.java
+++ src/main/java/Algorithm/OAClassifyApplication.java
@@ -1,25 +1,27 @@
 package Algorithm;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
 
 import weka.classifiers.Classifier;
-import weka.classifiers.bayes.NaiveBayes;
-import weka.classifiers.pmml.consumer.NeuralNetwork;
-import weka.classifiers.trees.J48;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.lazy.IBk;
 import weka.classifiers.trees.RandomForest;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
 import weka.core.converters.ArffLoader;
+import weka.core.converters.ArffSaver;
 import weka.filters.Filter;
-import weka.filters.unsupervised.attribute.StringToNominal;
+import weka.filters.unsupervised.attribute.StringToWordVector;
 
 public class OAClassifyApplication {
 
-    private static String trainName = "/Users/xiaoxiangyuzhu/Desktop/project/testdata/oa.arff";
-    private static String testName = "/Users/xiaoxiangyuzhu/Desktop/project/testdata/oa_test.arff";
-    //读取arff文件,传入instances
+    private static String trainName = "/Users/xiaoxiangyuzhu/Desktop/project/testdata/labor.arff";
+    private static String testName = "/Users/xiaoxiangyuzhu/Desktop/project/testdata/labor_test.arff";
+    private static String fileName = "/Users/xiaoxiangyuzhu/Desktop/project/testdata/labor_classify.arff";
+
+    //读取生arff文件,将内容传入实例instances
     public static Instances getRawInstancesByFilename(String filename) throws IOException {
         Instances instances = null;
         try {
@@ -32,57 +34,94 @@ public class OAClassifyApplication {
         }
         return instances;
     }
-    //转换文件中的字符串类型为Nominal类型
+
+    //转换实例文件中的字符串类型为wordtovector，使得符合classifier的requisition
     public static Instances getOldInstancesByRaw(Instances ins) throws Exception {  // throws IOException
         Instances instances = null;
-        StringToNominal stringToNominal=new StringToNominal();
+        StringToWordVector filter = new StringToWordVector();
         try {
-            stringToNominal.setInputFormat(ins);
-            instances = Filter.useFilter(ins,stringToNominal);
-
-        }catch (Exception e){
+            filter.setIDFTransform(true);
+            filter.setTFTransform(true);
+            filter.setInputFormat(ins);
+            instances = Filter.useFilter(ins, filter);
+        } catch (Exception e) {
             System.err.println(e.getStackTrace());
         }
         return instances;
     }
 
-    public static void main(String[] args) throws Exception {
-        Classifier m_classifier = new NaiveBayes();//RandomForest()
-//        File inputFile = new File("/Users/xiaoxiangyuzhu/Desktop/project/testdata/labor.arff");//训练语料文件
-//        ArffLoader atf = new ArffLoader();
-//        atf.setFile(inputFile);
-//        Instances instancesTrain = atf.getDataSet(); // 读入训练文件
-        Instances instancesTrain = getRawInstancesByFilename(trainName);
-        instancesTrain = getOldInstancesByRaw(instancesTrain);
-//        inputFile = new File("/Users/xiaoxiangyuzhu/Desktop/project/testdata/labor_test.arff");//测试语料文件
-//        atf.setFile(inputFile);
-//        Instances instancesTest = atf.getDataSet(); // 读入测试文件
-        Instances instancesTest = getRawInstancesByFilename(testName);
-        instancesTest = getOldInstancesByRaw(instancesTest);
+    //训练并保存模型
+    public static void trainModel(Instances instances, Instances instancesTest, Classifier classifier, String modelname) throws Exception {
+        try {
+            //训练：设置类标位置
+            instances.setClassIndex(0);
+            instancesTest.setClassIndex(0); //设置分类属性所在行号（第一行为0号），属性总数instancesTrain.numAttributes()-1
+            //训练模型
+            classifier.buildClassifier(instances);
+            //保存模型
+            SerializationHelper.write("target/" + modelname + ".model", classifier);
 
-        //训练模型
-        instancesTrain.setClassIndex(0);
-        m_classifier.buildClassifier(instancesTrain); //训练
-        System.out.println(m_classifier);
-
-        // 保存模型
-        SerializationHelper.write("target/classifier.model", m_classifier);//参数一为模型保存文件，classifier4为要保存的模型
-
-        //测试集属性
-        instancesTest.setClassIndex(0); //设置分类属性所在行号（第一行为0号），属性总数instancesTrain.numAttributes()-1
-        double sum = instancesTest.numInstances();//测试语料实例数
-        double right = 0.0f;
-
-        // 获取上面保存的模型
-        for (int i = 0; i < sum; i++)//测试分类结果  1
-        {
-            if (m_classifier.classifyInstance(instancesTest.instance(i)) == instancesTest.instance(i).classValue())//如果预测值和答案值相等（测试语料中的分类列提供的须为正确答案，结果才有意义）
+            //测试：测试集构建
+            double sum = instancesTest.numInstances();//测试语料实例数
+            double right = 0.0f;
+            // 获取上面保存的模型
+            for (int i = 0; i < sum; i++)//测试分类结果  1
             {
-                right++;//正确值加1
+                if (classifier.classifyInstance(instancesTest.instance(i)) == instancesTest.instance(i).classValue())//如果预测值和答案值相等（测试语料中的分类列提供的须为正确答案，结果才有意义）
+                {
+                    right++;//正确值加1
+                }
             }
+            System.out.println(right);
+            System.out.println(sum);
+            System.out.println(modelname + "classification precision:" + (right / sum));
+        } catch (Exception e) {
+            System.err.println(e.getStackTrace());
         }
-        System.out.println(right);
-        System.out.println(sum);
-        System.out.println("RandomForest classification precision:" + (right / sum));
+    }
+
+    //分类
+    public static void classifyFile(Classifier classifier, String fileName) throws Exception {
+        try {
+            Instances instancesRaw = getRawInstancesByFilename(fileName);
+            Instances instances = getOldInstancesByRaw(instancesRaw);
+            double sum = instances.numInstances();
+            for (int i = 0; i < sum; i++)//测试分类结果
+            {
+                instancesRaw.instance(i).setClassValue(classifier.classifyInstance(instances.instance(i)));
+            }
+            ArffSaver saver = new ArffSaver();
+            saver.setInstances(instancesRaw);
+            saver.setFile(new File(fileName));
+            saver.writeBatch();
+        } catch (Exception e) {
+            System.err.println(e.getStackTrace());
+        }
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        //定义多个分类模型
+        Classifier m_classifier = new MultilayerPerceptron();
+        Classifier r_classifier = new RandomForest();
+        Classifier i_classifier = new IBk(3);
+
+        //获取并处理训练文件：StringtoWordVector
+        Instances instancesTrainRaw = getRawInstancesByFilename(trainName);
+        Instances instancesTrain = getOldInstancesByRaw(instancesTrainRaw);
+
+        //获取并处理测试文件：StringtoWordVector
+        Instances instancesTestRaw = getRawInstancesByFilename(testName);
+        Instances instancesTest = getOldInstancesByRaw(instancesTestRaw);
+
+        //训练模型并输出模型及效果
+        //trainModel(instancesTrain, instancesTest, m_classifier, "MultilayerPerceptron");
+        trainModel(instancesTrain, instancesTest, r_classifier, "RandomForest");
+        //trainModel(instancesTrain, instancesTest, i_classifier, "IBk-3");
+
+        //进行实际分类
+        classifyFile(r_classifier, fileName);
     }
 }
